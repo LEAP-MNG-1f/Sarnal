5b6ea538ddd5e58dda0d0950d71f581912cdc09b
sasuke <sasuke@sasukes-MacBook-Air.local>
2024-11-04 16:23:32 +0800

    post, put, get, delete methods hiiv

--- back-end/index.js
+++ back-end/index.js
@@ -2,36 +2,65 @@ import express, { request, response } from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 9005;
+const port = 9009;
 
 app.use(bodyParser.json());
 
 const users=[
-{name: "Sarnai", age:99},
-{name: "Bolroo", age:18},
-];
+{id:1, name: "Haimish", age:34},
+{id:2, name: "Guyug", age:35},
+{id:3, name: "Eguunee", age:466},
+{id:4, name: "Taishir", age:546},
 
+];
 
 
 app.get("/users", (request,response)=>{
     response.send(users);
-    console.log("get shuu za");
+    console.log("4 hereglegch get huselteer avlaa");
 });
 
 app.post("/users", (request, response)=>{
-    response.send("hello post huselt avlaa");
-    console.log("post shuu za");
+    const {name, age }=request.body;
+      
+    const number = Math.random()*100;
+    const result = Math.floor(number);
+    const newUser={
+        id: result,
+        name: name,
+        age: age
+    }
+
+    users.push(newUser)
+    response.send(users)
 });
 
 app.put("/users", (request,response)=>{
-    response.send("hello put huselt irlee");
-    console.log("put shuu za");
+    const {id, updatedName, updatedAge}=request.body;
+    if (!id){
+        return response.send("ID baihgui bna");
+    }
+    if (!updatedName && !updatedAge){
+        return response.send("gomen!!!! name, age oruulaagui bna!!!!")
+    }
+    
+    users.find((user)=>{
+        if (user.id==id){
+            user.name = updatedName ? updatedName : user.name
+            user.age = updatedAge ? updatedAge : user.age
+            return user;
+        } 
+    });
+    response.send(users);
+    console.log("Edit hiigdlee");
 });
 
+
 app.delete("/users", (request,response)=>{
-    response.send("hello delete huselt irlee");
-    console.log("delete shuu za");
-});
+    const {id}=request.body;
+    const filteredUser = users.filter((user)=>user.id!==id);
+    response.json(filteredUser);
+    });
 
 
 app.listen(port ,()=>{
